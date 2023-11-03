df40353afb3f403935a2f9a0c316a6df4a273ad8
Mihail Palenov <m.e.palenov@gmail.com>
2023-11-03 23:16:41 +0300

    change for 2 coords

--- Ball.py
+++ Ball.py
@@ -1,5 +1,5 @@
 """Создаём програмудля прыгающих шаров"""
-
+from Vector import Vector
 
 class Ball():
     """Шар родитель, основной класс"""
@@ -9,16 +9,42 @@ class Ball():
     default_color = 'red'
 
 
-    def __init__(self, rigidity, InitPos, color):
+    def __init__(self, rigidity, init_pos_x, init_pos_y, speed_dir_x, speed_dir_y, color):
         """ Initate our ball """
         # self.diameter = diameter
         # self.weight = weight
         self.rigidity = rigidity
-        self.InitPos = InitPos
+        self.init_pos = Vector(init_pos_x, init_pos_y)
+        self.speed_dir = Vector(speed_dir_x, speed_dir_y)
         self.color = color
-        
+    
+    def show_Ball(self):
+        """Показать характеристики шара"""
+        print(f'Жёсткость (прыгучесть) шара - {self.rigidity}')
+        print(f'Координаты шара: (x0={self.init_pos.initial_x}, y0={self.init_pos.initial_y})')
+        print(f'Вектор скорости: x={self.speed_dir.initial_x}, y={self.speed_dir.initial_y}')
+        print()
 
-    # def movement(self):
-    #     """Шар меняет своё положение в пространстве"""
-    #     self.coord_x += vector_x
-    #     self.coord_y += vector_y - g
+    def set_speed_dir(self, new_speed_dir_x, new_speed_dir_y):
+        """Устанавливаем новый родной вектор скорости шара"""
+        self.speed_dir.initial_x = new_speed_dir_x
+        self.speed_dir.initial_y = new_speed_dir_y
+        print()  
+
+    def movement(self):
+        """Шар меняет своё положение в пространстве"""
+        self.init_pos_x.initial_x += self.speed_dir_x.initial_x
+        self.init_pos_y.initial_y += self.speed_dir_y.initial_y - 1
+        print(f'Координаты шара теперь: (x0={self.init_pos_x}, y0={self.init_pos_y})')
+        print()
+
+
+sha = Ball(1, 2, 2, 3, 3, 'yellow')
+
+sha.show_Ball()
+
+dae = Vector(1, 3)
+
+sha.set_speed_dir(1, 3)
+
+sha.show_Ball()
--- Vector.py
+++ Vector.py
@@ -3,47 +3,82 @@
 class Vector():
     """Создаём любой вектор"""
 
-    def __init__(self, initial_x, initial_y, finite_x, finite_y):
+    def __init__(self, initial_x, initial_y):
+        """Задаём параметры нашего вектора"""
         self.initial_x = initial_x
         self.initial_y = initial_y
-        self.finite_x = finite_x
-        self.finite_y = finite_y
         
-    
     def show_vector(self):
-        print(f'Начальные координаты: (x0={self.initial_x}, y0={self.initial_y})')
-        print(f'Начальные координаты: (x0={self.finite_x}, y0={self.finite_y})')
+        print(f'Координаты вектора: (x0={self.initial_x}, y0={self.initial_y})')
         print()
 
-    def set_x(self, new_initial_x, new_finite_x):
-        """Задать новые начальные и коенчные координаты Х вектора"""
+    def set_coord(self, new_initial_x,new_initial_y):
+        """Задать новые начальные и коенчные координаты x вектора"""
         self.initial_x = new_initial_x
-        self.finite_x = new_finite_x
-    
-    def set_y(self, new_initial_y, new_finite_y):
-        """Задать новые начальные и коенчные координаты Y вектора"""
         self.initial_y = new_initial_y
-        self.finite_y = new_finite_y
 
-    def vector_length(self):
-        """Показать длину вектора"""
-        print(f'Длина вектора: {round(float(((self.finite_x - self.initial_x)**2 + (self.finite_y - self.initial_y)**2)**0.5), 2)}')
-
-    def sum_vectors(self, initial_x2, initial_y2, finite_x2, finite_y2):
+    def sum_vectors(self, initial_x2, initial_y2):
         """Прибавить вектор к существующему"""
-        self.finite_x += finite_x2 - initial_x2
-        self.finite_y += finite_y2 - initial_y2
-
-    def diff_vectors(self, initial_x2, initial_y2, finite_x2, finite_y2):
-        """Вычесть вектор из существующего"""
-        self.initial_x += finite_x2 - initial_x2
-        self.initial_y += finite_y2 - initial_y2
+        self.initial_x += initial_x2
+        self.initial_y += initial_y2
+        print()
 
     def mult_vector(self, factor):
         """Умножить вектор на число"""
-        self.finite_x = self.initial_x + (self.finite_x - self.initial_x) * factor
-        self.finite_x = self.initial_y + (self.finite_y - self.initial_y) * factor
+        self.initial_x = self.initial_x * factor
+        self.initial_y = self.initial_y * factor
+        print()
+
+    # def vector_length(self):
+    #     """Показать длину вектора"""
+    #     print(f'Длина вектора: {round(float(((self.finite_x - self.initial_x)**2 + (self.finite_y - self.initial_y)**2)**0.5), 2)}')
+    #     print()
         
+kuka = Vector(2, 5)
 
+print(kuka.initial_x)
 
-kuka = Vector(1, 1, 5, 5)
+# class Vector():
+#     """Создаём любой вектор"""
+
+#     def __init__(self, initial_x, initial_y, finite_x, finite_y):
+#         """Задаём параметры нашего вектора"""
+#         self.initial_x = initial_x
+#         self.initial_y = initial_y
+#         self.finite_x = finite_x
+#         self.finite_y = finite_y
+        
+#     def show_vector(self):
+#         print(f'Начальные координаты: (x0={self.initial_x}, y0={self.initial_y})')
+#         print(f'Начальные координаты: (x0={self.finite_x}, y0={self.finite_y})')
+#         print()
+
+#     def set_x(self, new_initial_x, new_finite_x):
+#         """Задать новые начальные и коенчные координаты Х вектора"""
+#         self.initial_x = new_initial_x
+#         self.finite_x = new_finite_x
+    
+#     def set_y(self, new_initial_y, new_finite_y):
+#         """Задать новые начальные и коенчные координаты Y вектора"""
+#         self.initial_y = new_initial_y
+#         self.finite_y = new_finite_y
+
+#     def vector_length(self):
+#         """Показать длину вектора"""
+#         print(f'Длина вектора: {round(float(((self.finite_x - self.initial_x)**2 + (self.finite_y - self.initial_y)**2)**0.5), 2)}')
+
+#     def sum_vectors(self, initial_x2, initial_y2, finite_x2, finite_y2):
+#         """Прибавить вектор к существующему"""
+#         self.finite_x += finite_x2 - initial_x2
+#         self.finite_y += finite_y2 - initial_y2
+
+#     def diff_vectors(self, initial_x2, initial_y2, finite_x2, finite_y2):
+#         """Вычесть вектор из существующего"""
+#         self.initial_x += finite_x2 - initial_x2
+#         self.initial_y += finite_y2 - initial_y2
+
+#     def mult_vector(self, factor):
+#         """Умножить вектор на число"""
+#         self.finite_x = self.initial_x + (self.finite_x - self.initial_x) * factor
+#         self.finite_x = self.initial_y + (self.finite_y - self.initial_y) * factor
+        
